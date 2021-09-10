d6d3fea46c4dfbb730fce1a58f4db49a13d16e3a
OrrMatzkin <orr.matzkin@gmail.com>
2021-09-10 12:27:18 +0300

    passed tests

--- src/oop/ex5/main/Sjavac.java
+++ src/oop/ex5/main/Sjavac.java
@@ -1,8 +1,8 @@
 package oop.ex5.main;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
-import java.util.zip.DataFormatException;
 
 /**
  * This class operates the entire Simplified Java Verifier program.
@@ -13,60 +13,81 @@ import java.util.zip.DataFormatException;
 public class Sjavac {
 
     /**
+     * Enum for the output type, no value field needed because it's redundant.
+     * (we will use the 'ordinal()' method to get the numeric value.
+     */
+    private enum OutputType {
+        LEGAL(), ILLEGAL(), IO_ERROR();
+    }
+
+    /**
      * The main method of the program.
-     * @param args The arguments given in the CMD, in order to run the program.
+     * @param args The arguments given in the command line in order to run the program.
      */
     public static void main(String[] args) {
         try {
             if (args.length < 1) throw new IllegalArgumentException("Missing s-Java file name.");
             else if (args.length > 1) throw new IllegalArgumentException("Too many arguments.");
             List<String> fileContent = getSjavaLines(args[0]);
-            CallsHandler callsHandler = CallsHandler.getSingleInstance();
             Scope scope = new Scope(fileContent, null, "Global Scope");
             scope.scan();
-            callsHandler.callValidity();
-            GlobalVariablesChecker.checkGlobalAssignments();
-            GlobalVariablesChecker.checkGlobalDeclaration();
-            GlobalVariablesChecker.checkGlobalCondition();
+            finalChecks();
         }
-        catch (DataFormatException | FileNotFoundException | IllegalArgumentException e ) {
-            System.out.println("2");
-            e.printStackTrace();
+        catch (IOException | IllegalArgumentException e) {
+            System.out.println(OutputType.IO_ERROR.ordinal());
             System.err.println(e);
-
             return;
         }
         catch (VariableError | ScopeError | MethodError e) {
-            System.out.println("1");
-            e.printStackTrace();
+            System.out.println(OutputType.ILLEGAL.ordinal());
             System.err.println(e);
             return;
 
         } finally {
-            Variable.existingVariables.clear();
-            Variable.existingArguments.clear();
-            Method.allMethods.clear();
-            CallsHandler.calls.clear();
-            GlobalVariablesChecker.globalVariablesAssignments.clear();
-            GlobalVariablesChecker.globalVariablesDeclaration.clear();
-            GlobalVariablesChecker.globalVariablesCondition.clear();
-            Scope.globalScope = null;
+            clearStaticCollections();
         }
-        System.out.println("0");
+        System.out.println(OutputType.LEGAL.ordinal());
     }
 
     /**
      * Gets the entire file lines in order.
      * @param filePath The source Sjava file path.
      * @return List of all the lines of the file.
-     * @throws DataFormatException If the data format is not valid.
-     * @throws FileNotFoundException If the file does not exist.
+     * @throws IOException If the file does not exist.
      */
     private static List<String> getSjavaLines(String filePath)
-            throws DataFormatException, FileNotFoundException {
-        Reader reader = new Reader(filePath);
-        reader.readFile();
-        return reader.getFileContent();
+            throws IOException {
+        SjavaFileReader sjavaFileReader = new SjavaFileReader(filePath);
+        sjavaFileReader.readFile();
+        return sjavaFileReader.getFileContent();
+    }
+
+    /**
+     * Clears the static collection members from all class.
+     */
+    private static void clearStaticCollections(){
+        Variable.existingVariables.clear();
+        Variable.existingArguments.clear();
+        Method.allMethods.clear();
+        MethodCallsChecker.calls.clear();
+        GlobalVariablesChecker.globalVariablesAssignments.clear();
+        GlobalVariablesChecker.globalVariablesDeclaration.clear();
+        GlobalVariablesChecker.globalVariablesCondition.clear();
+        Scope.globalScope = null;
+    }
+
+    /**
+     * Runs the final check for method call, and global assignments, declarations and conditions.
+     * Some methods and variables might be declared after their usage.
+     * @throws ScopeError If there is Scope error.
+     * @throws MethodError If there is Method error.
+     * @throws VariableError If there is Variable error.
+     */
+    private static void finalChecks() throws ScopeError, MethodError, VariableError {
+        MethodCallsChecker.CheckCalls();
+        GlobalVariablesChecker.checkGlobalAssignments();
+        GlobalVariablesChecker.checkGlobalDeclaration();
+        GlobalVariablesChecker.checkGlobalCondition();
     }
 
 }
